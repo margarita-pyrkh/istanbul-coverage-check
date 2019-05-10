f52eeb265c3f7762007b99a4aa728a197fddbc65
Margarita Pyrkh <ritapyrh07@gmail.com>
2019-05-10 15:58:52 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'istanbul-coverage-test';
+  title2 = 'istanbul-coverage-test';
+}
